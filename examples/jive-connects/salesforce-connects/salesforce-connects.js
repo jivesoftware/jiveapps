f2dd7916c7170f1e1f349f84b8c2aed4d2d05727
craigmcc <craig.mcclanahan@jivesoftware.com>
2011-01-12 00:00:16 -0800

    Initial versions of three Jive Connects API examples.

---
+++ examples/jive-connects/salesforce-connects/salesforce-connects.js
@@ -0,0 +1,325 @@
+// Symbolic constant for our connection alias
+var ALIAS = "salesforce";
+
+// Currently selected account
+var account;
+
+// Accounts that we loaded
+var accounts = [ ];
+
+// Currently selected contact
+var contact;
+
+// Contacts for the currently selected account
+var contacts = [ ];
+
+// Opportunities for the currently selected account
+var opportunities = [ ];
+
+// Currently selected opportunity
+var opportunity;
+
+// Path prefix (to be looked up on the initial call)
+var prefix = "";
+
+// Hash of resource URL partial paths, keyed by resource name
+var resources = { };
+
+// The currently logged in user
+var user;
+
+// On-view-load initialization
+function init() {
+    registerHandlers();
+    loadUser();
+}
+
+// Load and cache the results of a query to retrieve information about acocunts
+function loadAccounts() {
+    console.log("loadAccounts() started");
+    showMessage("Loading current accounts ...");
+    osapi.jive.connects.get({
+        alias : ALIAS,
+        href : resources.query,
+        params : { q : 'SELECT Id, Name, Type, BillingCity, BillingState FROM Account ORDER BY Name' }
+    }).execute(function (response) {
+        console.log("loadAccounts() response = " + JSON.stringify(response));
+        if (response.error) {
+            if (response.error.code == 401) {
+                console.log("Received a 401 response, triggering reconfiguration");
+                osapi.jive.connects.reconfigure(ALIAS, response, function(feedback) {
+                    console.log("Received reconfigure feedback " + JSON.stringify(feedback));
+                    loadAccounts();
+                })
+            }
+            else {
+                alert("Error " + response.error.code + " loading data: " + response.error.message);
+            }
+        }
+        else {
+            accounts = response.content.records;
+            var html = "";
+            $.each(accounts, function(index, account) {
+                html += '<hr /><p><strong>' + account["Name"] + '</strong></p>';
+                html += '<p>Location: ' + account["BillingCity"] + ', ' + account["BillingState"] + '</p>';
+                html += '<p>Type: ' + account["Type"] + '</p>';
+                html += '<p>Links:';
+                html += ' <a class="contacts-link" href="#" data-index="' + index + '">Contacts</a>';
+                html += ' <a class="opportunities-link" href="#" data-index="' + index + '">Opportunities</a>';
+                html += '</p>';
+            });
+            $("#accounts-list").html("").html(html);
+            $(".contacts-link").click(function() {
+                var index = $(this).attr("data-index");
+                account = accounts[index];
+                $(".account-name").html("").html(account["Name"]);
+                loadContacts();
+            });
+            $(".opportunities-link").click(function() {
+                var index = $(this).attr("data-index");
+                account = accounts[index];
+                $(".account-name").html("").html(account["Name"]);
+                loadOpportunities();
+            });
+            showOnly("accounts-div");
+            gadgets.window.adjustHeight();
+        }
+    });
+}
+
+// Load contacts for the currently selected account
+function loadContacts() {
+    console.log("loadContacts() started");
+    showMessage("Loading contacts for selected account ...");
+    osapi.jive.connects.get({
+        alias : ALIAS,
+        href : resources.query,
+        params : { q : "SELECT Id, FirstName, LastName, Email, Phone FROM Contact WHERE AccountId='" +
+                       account["Id"] + "' ORDER BY LastName, FirstName" }
+    }).execute(function (response) {
+        console.log("loadContacts() response = " + JSON.stringify(response));
+        if (response.error) {
+            if (response.error.code == 401) {
+                console.log("Received a 401 response, triggering reconfiguration");
+                osapi.jive.connects.reconfigure(ALIAS, response, function(feedback) {
+                    console.log("Received reconfigure feedback " + JSON.stringify(feedback));
+                    loadContacts();
+                })
+            }
+            else {
+                alert("Error " + response.error.code + " loading data: " + response.error.message);
+            }
+        }
+        else {
+            contacts = response.content.records;
+            var html = "";
+            $.each(contacts, function(index, contact) {
+                html += '<hr />';
+                html += '<p><strong>' + contact["LastName"] + ', ' + contact["FirstName"] + '</strong></p>';
+                html += '<p>Email: ' + contact["Email"] + '</p>';
+                html += '<p>Phone: ' + contact["Phone"] + '</p>';
+                html += '<p>Links:';
+                html += ' <a class="call-link" href="#" data-index="' + index + '">Make The Call!</a>';
+                html += '</p>';
+            });
+            $("#contacts-list").html("").html(html);
+            $(".call-link").click(function() {
+                var index = $(this).attr("data-index");
+                contact = contacts[index];
+                var entry = {
+                    activity : {
+                        body : '{@actor} needs to contact <b>' + contact["FirstName"] + ' ' + contact["LastName"]
+                               + '</b> by phone at ' + contact["Phone"]
+                               + ' or by email at ' + contact["Email"]
+                               + ' related to account ' + account["Name"] + '.',
+                        object : {
+                            actionLinks : [
+                                { title : 'Done' }
+                            ],
+                            summary : 'This is the <b>summary</b> <i>part</i>.'
+                        },
+                        title : 'Make The Call!'
+                    },
+                    deliverTo : [ user.id ]
+                };
+                console.log("Creating action = " + JSON.stringify(entry));
+                osapi.activities.create(entry).execute(function(response) {
+                    console.log("creating action response = " + JSON.stringify(response));
+                    alert("Created action");
+                });
+            });
+            showOnly("contacts-div");
+            gadgets.window.adjustHeight();
+        }
+    });
+}
+
+// Load opportunities for the currently selected account
+function loadOpportunities() {
+    console.log("loadOpportunities() started");
+    showMessage("Loading opportunities for selected account ...");
+    osapi.jive.connects.get({
+        alias : ALIAS,
+        href : resources.query,
+        params : { q : "SELECT Id, Name, StageName, Amount FROM Opportunity WHERE AccountId='" +
+                       account["Id"] + "' ORDER BY Name" }
+    }).execute(function (response) {
+        console.log("loadOpportunities() response = " + JSON.stringify(response));
+        if (response.error) {
+            if (response.error.code == 401) {
+                console.log("Received a 401 response, triggering reconfiguration");
+                osapi.jive.connects.reconfigure(ALIAS, response, function(feedback) {
+                    console.log("Received reconfigure feedback " + JSON.stringify(feedback));
+                    loadOpportunities();
+                })
+            }
+            else {
+                alert("Error " + response.error.code + " loading data: " + response.error.message);
+            }
+        }
+        else {
+            opportunities = response.content.records;
+            var html = "";
+            $.each(opportunities, function(index, opportunity) {
+                html += '<hr />';
+                html += '<p><strong>' + opportunity["Name"] + '</strong></p>';
+                html += '<p>Stage: ' + opportunity["StageName"] + '</p>';
+                html += '<p>Amount: ' + opportunity["Amount"] + '</p>';
+                html += '<p>Links:';
+                html += ' <a class="reviewed-link" href="#" data-index="' + index + '" data-verb="Accepted">Mark as Accepted</a>';
+                html += '&nbsp;';
+                html += ' <a class="reviewed-link" href="#" data-index="' + index + '" data-verb="Rejected">Mark as Rejected</a>';
+                html += '</p>';
+            });
+            $("#opportunities-list").html("").html(html);
+            $(".reviewed-link").click(function() {
+                var index = $(this).attr("data-index");
+                var verb = $(this).attr("data-verb");
+                opportunity = opportunities[index];
+                var entry = {
+                    activity : {
+                        body : '{@actor} reviewed an opportunity',
+                        object : {
+                            summary : 'The reviewed opportunity was <b>' + opportunity["Name"]
+                                    + '</b> for account <i>' + account["Name"] + '</i>.  '
+                        },
+                        title : verb + ' An Opportunity Review',
+                        verb : verb
+                    }
+                };
+                console.log("Creating activity stream entry = " + JSON.stringify(entry));
+                osapi.activities.create(entry).execute(function(response) {
+                    console.log("creating activity stream entry response = " + JSON.stringify(response));
+                    alert("Created an activity stream entry");
+                });
+            });
+            showOnly("opportunities-div");
+            gadgets.window.adjustHeight();
+        }
+    });
+}
+
+// Load and cache the path prefix
+function loadPrefix() {
+    console.log("loadPrefix() started");
+    showMessage("Loading SalesForce.Com version information ...");
+    osapi.jive.connects.get({
+        alias : ALIAS,
+        href : '/services/data'
+    }).execute(function (response) {
+        console.log("loadPrefix() response = " + JSON.stringify(response));
+        if (response.error) {
+            if (response.error.code == 401) {
+                console.log("Received a 401 response, triggering reconfiguration");
+                osapi.jive.connects.reconfigure(ALIAS, response, function(feedback) {
+                    console.log("Received reconfigure feedback " + JSON.stringify(feedback));
+                    loadPrefix();
+                })
+            }
+            else {
+                alert("Error " + response.error.code + " loading data: " + response.error.message);
+            }
+        }
+        else {
+            // Use the first returned API version by default
+            console.log("loadPrefix() setting prefix to '" + response.content[0].url + "'");
+            prefix = response.content[0].url;
+            loadResources();
+        }
+    });
+}
+
+// Load the currently logged in user
+function loadUser() {
+    console.log("loadUser() started");
+    showMessage("Loading the currently logged in user ...");
+    osapi.people.get({
+        userId : '@me'
+    }).execute(function(response) {
+        console.log("loadUser() response = " + JSON.stringify(response));
+        if (response.error) {
+            alert("Error " + response.error.code + " getting current user: " + response.error.message);
+        }
+        else {
+            if (response instanceof Array) {
+                user = response[0];
+            }
+            else {
+                user = response;
+            }
+            loadPrefix();
+        }
+    })
+}
+
+// Load the available resource URLs that we can access
+function loadResources() {
+    console.log("loadResources() started");
+    showMessage("Loading SalesForce.Com resource links ...");
+    osapi.jive.connects.get({
+        alias : ALIAS,
+        href : prefix
+    }).execute(function (response) {
+        console.log("loadResources() response = " + JSON.stringify(response));
+        if (response.error) {
+            if (response.error.code == 401) {
+                console.log("Received a 401 response, triggering reconfiguration");
+                osapi.jive.connects.reconfigure(ALIAS, response, function(feedback) {
+                    console.log("Received reconfigure feedback " + JSON.stringify(feedback));
+                    loadResources();
+                })
+            }
+            else {
+                alert("Error " + response.error.code + " loading data: " + response.error.message);
+            }
+        }
+        else {
+            resources = response.content;
+            loadAccounts();
+        }
+
+    });
+}
+
+// Register UI event handlers
+function registerHandlers() {
+    $(".back-to-accounts").click(function() {
+        loadAccounts();
+    });
+}
+
+// Show the specified status message
+function showMessage(message) {
+    $("#status-message").html("").html(message);
+    showOnly("status-div");
+}
+
+// Show only the specified top level div
+function showOnly(id) {
+    $(".top-level-div").hide();
+    $("#" + id).show();
+    gadgets.window.adjustHeight();
+}
+
+// Register our on-view-load handler
+gadgets.util.registerOnLoadHandler(init);
---
+++ examples/jive-connects/salesforce-metadata/salesforce-metadata.js
@@ -0,0 +1,227 @@
+// Symbolic constant for our connection alias
+var ALIAS = "salesforce";
+
+// Selected object instance
+var object = { };
+
+// Array of metadata about available objects
+var objects = [ ];
+
+// Hash of resource URL partial paths, keyed by resource name
+var resources = { };
+
+// Selected version instance, with "label", "url", and "version" properties
+var version = { };
+
+// Array of version objects available in this SalesForce instance
+var versions = [ ];
+
+// On-view-load initialization
+function init() {
+    registerHandlers();
+    loadVersions();
+}
+
+// Load descriptive information about the fields for the selected object
+function loadObject() {
+    showMessage("Loading field information for the selected system object ...");
+    console.log("loadObject() started");
+    osapi.jive.connects.get({
+        alias : ALIAS,
+        href : object.urls.describe
+    }).execute(function (response) {
+        console.log("loadObject() response = " + JSON.stringify(response));
+        if (response.error) {
+            if (response.error.code == 401) {
+                console.log("Received a 401 response, triggering reconfiguration");
+                osapi.jive.connects.reconfigure(ALIAS, response, function(feedback) {
+                    console.log("Received reconfigure feedback " + JSON.stringify(feedback));
+                    loadDescribe();
+                })
+            }
+            else {
+                alert("Error " + response.error.code + " loading data: " + response.error.message);
+            }
+        }
+        else {
+            $(".object-title").html("").html(object.name);
+            var fields = response.content.fields;
+            var html = "";
+            $.each(fields, function(index, f) {
+                if (!f.deprecatedAndHidden) {
+                    html += '<tr>';
+                    html += '<td>' + f.name + '</td>';
+                    html += '<td>' + f.label + '</td>';
+                    html += '<td>' + f.type + '</td>';
+                    html += '<td>' + f.length + '</td>';
+                    html += '<td>' + f.digits + '</td>';
+                    html += '<td>' + f.caseSensitive + '</td>';
+                    html += '<td>' + f.filterable + '</td>';
+                    html += '<td>' + f.nillable + '</td>';
+                    html += '<td>' + f.sortable + '</td>';
+                    html += '<td>' + f.updateable + '</td>';
+                    html += '</tr>';
+                }
+            });
+            $("#object-table-body").html("").html(html);
+            var relationships = response.content.childRelationships;
+            html = "";
+            $.each(relationships, function(index, r) {
+                if (!r.deprecatedAndHidden) {
+                    html += '<tr>';
+                    html += '<td>' + r.childSObject + '</td>';
+                    html += '<td>' + r.field + '</td>';
+                    html += '<td>' + r.cascadeDelete + '</td>';
+                    html += '<td>' + (r.relationshipName ? r.relationshipName : '&nbsp;') + '</td>';
+                    html += '</tr>';
+                }
+            });
+            $("#relationship-table-body").html("").html(html);
+            showOnly("object-div");
+        }
+    });
+}
+
+// Load the available object URLs that we can access
+function loadObjects() {
+    showMessage("Loading available system objects for this API version ...");
+    console.log("loadObjects() started");
+    osapi.jive.connects.get({
+        alias : ALIAS,
+        href : resources.sobjects
+    }).execute(function (response) {
+        console.log("loadObjects() response = " + JSON.stringify(response));
+        if (response.error) {
+            if (response.error.code == 401) {
+                console.log("Received a 401 response, triggering reconfiguration");
+                osapi.jive.connects.reconfigure(ALIAS, response, function(feedback) {
+                    console.log("Received reconfigure feedback " + JSON.stringify(feedback));
+                    loadObjects();
+                })
+            }
+            else {
+                alert("Error " + response.error.code + " loading data: " + response.error.message);
+            }
+        }
+        else {
+            objects = response.content.sobjects;
+            var html = "";
+            $.each(objects, function(index, o) {
+                if (!o.deprecatedAndHidden) {
+                    html += '<tr>';
+                    html += '<td><a class="object-link" href="#" data-index="' + index + '">' + o.name + '</a></td>';
+                    html += '<td>' + o.label + '</td>';
+                    html += '<td>' + o.createable + '</td>';
+                    html += '<td>' + o.deletable + '</td>';
+                    html += '<td>' + o.queryable + '</td>';
+                    html += '<td>' + o.retrieveable + '</td>';
+                    html += '<td>' + o.updateable + '</td>';
+                    html += '</tr>';
+                }
+            });
+            $("#objects-table-body").html("").html(html);
+            $(".object-link").click(function() {
+                var index = $(this).attr("data-index");
+                object = objects[index];
+                loadObject();
+            });
+            showOnly("objects-div");
+        }
+    });
+}
+
+// Load the resource links we will need
+function loadResources() {
+    showMessage("Loading resource URLs for the selected API version ...");
+    console.log("loadResources() started");
+    osapi.jive.connects.get({
+        alias : ALIAS,
+        href : version.url
+    }).execute(function (response) {
+        console.log("loadResources() response = " + JSON.stringify(response));
+        if (response.error) {
+            if (response.error.code == 401) {
+                console.log("Received a 401 response, triggering reconfiguration");
+                osapi.jive.connects.reconfigure(ALIAS, response, function(feedback) {
+                    console.log("Received reconfigure feedback " + JSON.stringify(feedback));
+                    loadResources();
+                })
+            }
+            else {
+                alert("Error " + response.error.code + " loading data: " + response.error.message);
+            }
+        }
+        else {
+            resources = response.content;
+            loadObjects();
+        }
+
+    });
+}
+
+// Load the available API versions, and let the user select the desired one
+function loadVersions() {
+    showMessage("Loading available API versions ...");
+    console.log("loadVersions() started");
+    osapi.jive.connects.get({
+        alias : ALIAS,
+        href : '/services/data'
+    }).execute(function (response) {
+        console.log("loadVersions() response = " + JSON.stringify(response));
+        if (response.error) {
+            if (response.error.code == 401) {
+                console.log("Received a 401 response, triggering reconfiguration");
+                osapi.jive.connects.reconfigure(ALIAS, response, function(feedback) {
+                    console.log("Received reconfigure feedback " + JSON.stringify(feedback));
+                    loadVersions();
+                })
+            }
+            else {
+                alert("Error " + response.error.code + " loading data: " + response.error.message);
+            }
+        }
+        else {
+            versions = response.content;
+            var html = "";
+            $.each(versions, function(index, v) {
+                html += '<li class="version-item">'
+                      + '<a class="version-link" href="#" data-index="' + index + '">' + v.label + '</a>'
+                      + ' (Version ' + v.version + ')</li>';
+            });
+            $("#versions-list").html("").html(html);
+            $(".version-link").click(function() {
+                var index = $(this).attr("data-index");
+                version = versions[index];
+                $(".version-title").html("").html(version.label + ' (Version ' + version.version + ')');
+                loadResources();
+            });
+            showOnly("versions-div");
+        }
+    });
+}
+
+// Register UI event handlers
+function registerHandlers() {
+    $("#back-to-objects").click(function() {
+        loadObjects();
+    });
+    $("#back-to-versions").click(function() {
+        loadVersions();
+    });
+}
+
+// Show the specified status message
+function showMessage(message) {
+    $("#status-message").html("").html(message);
+    showOnly("status-div");
+}
+
+// Show only the specified top level div
+function showOnly(id) {
+    $(".top-level-div").hide();
+    $("#" + id).show();
+    gadgets.window.adjustHeight();
+}
+
+// Register our on-view-load handler
+gadgets.util.registerOnLoadHandler(init);
