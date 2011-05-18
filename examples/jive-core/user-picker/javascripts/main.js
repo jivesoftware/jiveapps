695f61d3e79827a6a54330e3ef42988c361fc14e
Scott Becker <scott.becker@jivesoftware.com>
2011-05-18 10:27:36 -0700

    only render email addresses in canvas view, to avoid generating scrollbars in home view

--- examples/jive-core/user-picker/javascripts/main.js
+++ examples/jive-core/user-picker/javascripts/main.js
@@ -16,17 +16,32 @@ function getUsersFromResponse(response) {
 }
 
 function renderUserTable(users) {
+  var currentView = gadgets.views.getCurrentView().getName();
+
+  // render header row
   var content = ""
+  content += "<tr>"
+  content += "<th>ID</th>";
+  content += "<th>Name</th>";
+  content += "<th>Username</th>";
+  // include email in canvas view
+  if (currentView == 'canvas' || currentView == 'default') // Jive 5 currently returns 'default' for the canvas view
+    content += "<th>Email</th>";
+  content += "</tr>";
+
+  // render user rows
   for(var i = 0; i < users.length; i++) {
     var user = users[i];
     content += "<tr>"
     content += "<td>" + user.id + "</td>";
     content += "<td>" + user.name + "</td>";
     content += "<td>" + user.username + "</td>";
-    content += "<td>" + user.email + "</td>";
+    // include email in canvas view
+    if (currentView == 'canvas' || currentView == 'default') // Jive 5 currently returns 'default' for the canvas view
+      content += "<td>" + user.email + "</td>";
     content += "</tr>";
   }
-   
+
   $("#user-content").html(content);
   $("#user-info").show();
   gadgets.window.adjustHeight();
