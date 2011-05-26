4e289f01dc6eb8eee37a97ee21733a3e9b05e190
Alex Wenckus <awenckus@gmail.com>
2011-05-26 11:56:11 -0700

    Updates example App now includes an accordion

--- examples/jive-core/updates/updates.js
+++ examples/jive-core/updates/updates.js
@@ -1,40 +1,55 @@
-function loadUpdates() {
-  osapi.jive.core.updates.get({limit: 10}).execute(function(response) {
-    if (!response.error) {
-      var updates = response.data;
-      var recentUpdates = document.getElementById('recent_updates');
-      while (recentUpdates.hasChildNodes()) {
-        recentUpdates.removeChild(recentUpdates.lastChild);
-      }
+function loadUpdates(updatesHtml, userId) {
 
-      for (i = 0; i < updates.length; i++) {
-        var update = updates[i];
-        var user = update.author;
-        var node = document.createElement('li');
-        node.innerHTML = "<strong>" + (user ? user.name + ": " : "") + "</strong>" +
-            update.content.text;
-        recentUpdates.appendChild(node);
-      }
-      gadgets.window.adjustHeight();
+  osapi.jive.core.updates.get({limit: 10, userId: userId}).execute(function(response) {
+    if (response.error) {
+      return;
     }
+    var updates = response.data;
+
+    var newUpdatesHtml = updatesHtml.clone().html('');
+    for (var i = 0; i < updates.length; i++) {
+      var update = updates[i];
+      var user = update.author;
+
+      newUpdatesHtml.append("<li><strong>" + (user ? user.name + ": " : "") + "</strong>" +
+          update.content.text + "</li>");
+    }
+
+    updatesHtml.replaceWith(newUpdatesHtml);
+    $("#updates-accordion").accordion("resize");
+    gadgets.window.adjustHeight();
   });
 }
 
+function accordionChange(event, ui) {
+  var userId;
+  var headerId = ui.newHeader.attr("id");
+  if (headerId == "all-updates-header") {
+    userId = null;
+  }
+  else if (headerId == "my-updates-header") {
+    userId = "@viewer"
+  }
+  ui.oldContent.find("li").remove();
+  loadUpdates($(ui.newContent).find("ul"), userId);
+}
+
 gadgets.util.registerOnLoadHandler(function() {
-  var form = document.getElementById('update_form');
+  var form = $('update-form');
   form.onsubmit = function() {
-    var html = document.getElementById('update_entry').value;
+    var html = $('update-entry').value();
     osapi.jive.core.updates.create({userId: '@viewer', update: {html: html}}).
         execute(function(response) {
       if (!response.error) {
-        var updateEntry = document.getElementById('update_entry');
-        updateEntry.value = '';
+        $('update-entry').value('');
         loadUpdates();
       }
     });
 
     return false;
   };
+  $("#updates-accordion").accordion({changestart: accordionChange});
+  $("#update-post").button();
 
-  loadUpdates();
+  loadUpdates($("#all-updates"), null);
 });
