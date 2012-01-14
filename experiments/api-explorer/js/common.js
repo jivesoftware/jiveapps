51ca26385fd9fb3abe39c3233a189ebbda2a3bd1
Craig McClanahan <craig.mcclanahan@jivesoftware.com>
2012-01-13 18:24:37 -0800

    Flesh out getSecurityGroups and getSecurityGroup on Security Groups Service page, fix up some metadata interpretation errors.

--- experiments/api-explorer/js/common.js
+++ experiments/api-explorer/js/common.js
@@ -1,5 +1,30 @@
 // Common JavaScript Functions and variables
 
+var data = opensocial.data.DataContext;
+var options = { };
+
+// Calculate and return the set of fields from the multiple select options for the specfied select element id
+// (It is assumed that @all is always first, and overrides any additional selections)
+function calculateFields(id) {
+    var values = $("#" + id).val();
+    console.log("calculateFields(" + JSON.stringify(values) + ")");
+    if (values[0] == '@all') {
+        return "@all";
+    }
+    var fields = '';
+    var first = true;
+    $(values).each(function(index, value) {
+        if (first) {
+            first = false;
+        }
+        else {
+            fields += ",";
+        }
+        fields += value;
+    });
+    return fields;
+}
+
 // Set the top level title and currently selected select element to the specified value
 function configureTopLevelNav(title, category) {
     $(".top-level-title").html("").html(title);
